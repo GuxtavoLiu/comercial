e9c0a3292e6b95a9262efe01b4a8a22b9f152911
Gustavo Liu <gustavo_santostr@hotmail.com>
2018-10-02 22:33:30 -0300

    Começando a guardar no git

---
+++ src/util/RetornaUsuarioLogado.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import banco.DAOGenerico;
+import modelo.Funcionario;
+import sun.text.normalizer.ICUBinary.Authenticate;
+
+public class RetornaUsuarioLogado {
+
+	public static Funcionario get() {
+
+		DAOGenerico<Funcionario> dao = new DAOGenerico<>(Funcionario.class);
+		Funcionario func = new Funcionario();
+
+		String nomeUsuario = "";
+
+		Authentication authentication = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication != null) {
+			Object obj = authentication.getPrincipal();
+			if (obj instanceof UserDetails) {
+
+				nomeUsuario = ((UserDetails) obj).getUsername();
+			}
+		}
+
+		List<Funcionario> lf = dao.buscarCondicao("email='" + nomeUsuario + "'");
+
+		if (lf.size() > 0) {
+
+			func = lf.get(0);
+		}
+
+		return func;
+	}
+
+}
